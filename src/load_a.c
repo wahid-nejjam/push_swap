5fe568761455ebe2cf1833ac887363d438d2349a
corentin <corentin.noel@aliceadsl.fr>
2019-03-23 16:04:45 +0100

    saving

--- src/load_a.c
+++ src/load_a.c
@@ -6,7 +6,7 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 14:12:28 by conoel            #+#    #+#             */
-/*   Updated: 2019/03/23 12:43:47 by conoel           ###   ########.fr       */
+/*   Updated: 2019/03/23 15:59:21 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,8 @@ t_elem			*load_a(int argc, char **argv)
 	visu = ((argv[argc - 1][0] == 'c')) ? 1 : 0;
 	i = (argc == 2 || (argc == 3 && visu)) ? 0 : 1;
 	argc < 2 ? exit(-1) : 0;
-	(argc == 2 || (visu && argc == 3)) && ft_strchr(argv[1], ' ') ? argv = ft_strsplit(argv[1], ' ') : 0;
+	(argc == 2 || (visu && argc == 3)) && ft_strchr(argv[1], ' ') ? argv =
+		ft_strsplit(argv[1], ' ') : 0;
 	if (!(root_a = new(666, NULL, NULL, 1)))
 		return (NULL);
 	get_all(argc, argv, root_a, visu);
